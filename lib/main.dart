7dd8ca4438f033e13243bd829bcd3a29388a835f
gourteacher <ouaazs@algonquincollege.com>
2025-01-08 21:22:18 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+// CST2335 Week 3
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CST2335 Week 3',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Week 3 - ListView'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView(
+     // mainAxisAlignment: MainAxisAlignment.center,
+     // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListItemSection(name:"Montreal"),
+        ListItemSection(name:"Toronto"),
+        ListItemSection(name:"Vancouver"),
+        ListItemSection(name:"Calgary"),
+        ListItemSection(name:"Edmonton"),
+        ListItemSection(name:"Halifax"),
+        ListItemSection(name:"Winnipeg"),
+        ListItemSection(name:"Quebec City"),
+        ListItemSection(name:"Hamilton"),
+        ListItemSection(name:"Kitchener"),
+        ListItemSection(name:"London"),
+        ListItemSection(name:"Victoria"),
+        ListItemSection(name:"Oshawa"),
+        ListItemSection(name:"Windsor"),
+        ListItemSection(name:"Saskatoon"),
+        ListItemSection(name:"Regina"),
+        ListItemSection(name:"Sherbrooke"),
+        ListItemSection(name:"St. John's"),
+        ListItemSection(name:"Barrie"),
+        ListItemSection(name:"Ottawa"),
+      ],
+    );
+  }
+}
+
+class ListItemSection extends StatelessWidget {
+  const ListItemSection({super.key, required this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Text(
+        name,
+        style: const TextStyle(
+          color: Colors.blue,
+          decoration: TextDecoration.none,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+}
