c6b731e1314d3de0b4bc1d1634cb03e124714ad4
gourteacher <ouaazs@algonquincollege.com>
2025-03-06 20:42:19 -0500

    Week 9 - adding generated database file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'ToDoDAO.dart';
+import 'ToDoItem.dart';
+import 'database.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,10 +17,14 @@ class MyApp extends StatelessWidget {
       title: 'CST2335 Samples',
       theme: ThemeData(
         // This is the theme of your application.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+            secondary: Colors.green,
+            primary: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Week 6 - ListView'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Week 9 - Database'),
     );
   }
 }
@@ -33,78 +38,103 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var wordsArray = <String>[ ];
+  List<ToDoItem> todoList = <ToDoItem>[];
+  late TextEditingController _inputController;
+  late ToDoDAO taskDAO;
 
   @override //same as in java
   void initState() {
     super.initState(); //call the parent initState()
+    _inputController = TextEditingController();
+
+    $FloorToDoDatabase
+        .databaseBuilder('todo_database.db')
+        .build()
+        .then((database) async { //Note the use of async here
+      taskDAO = database.toDoDAO;
+      //get Items from database:
+      var it = await taskDAO.getAllItems();
+      setState(() {
+        todoList = it;
+      });
+    });
   }
 
   @override
-  void dispose()
-  {
+  void dispose() {
     super.dispose();
+    _inputController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar( backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
-        body: ListPage(),
-        floatingActionButton:
-        FloatingActionButton(onPressed: addItem,
-            tooltip: 'Add Item',
-            child: const Icon(Icons.add)
-        )
-    );
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title)),
+        body: listPage(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: addTask,
+            tooltip: 'Add a Task',
+            child: const Icon(Icons.add)));
   }
 
-  void addItem() {
-    setState(() {
-      wordsArray.add("Item " + " ${wordsArray.length+1}");
-    });
+  void addTask() {
+    if (_inputController.text.isNotEmpty) {
+      setState(() {
+        // Use this if Primary Key is not auto-generated
+        //ToDoItem(ToDoItem.ID++, _inputController.text);
+        var newItem = ToDoItem(null, _inputController.text);
+        todoList.add(newItem);
+        taskDAO.insertItem(newItem);
+        _inputController.clear();
+      });
+    } else {
+      var snackBar = SnackBar(content: Text('Input field is required'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
-  Widget ListPage(){
+  Widget listPage() {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(child:
-          ListView.builder(
-              itemCount:wordsArray.length,
-              itemBuilder: (context, rowNum) { return
-                Row( mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children:[
-                      Text("${1+rowNum}: ${wordsArray[rowNum]} "),
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            wordsArray[rowNum] = wordsArray[rowNum].toUpperCase();
-                          });
-                        },
-                        child: const Icon(Icons.arrow_upward),
-                      ),
-                      GestureDetector(
-                        onDoubleTap: () {
-                          setState(() {
-                            wordsArray[rowNum] = wordsArray[rowNum].toLowerCase();
-                          });
-                        },
-                        child: const Icon(Icons.arrow_downward),
-                      ),
-                      GestureDetector(
-                        onLongPress: () {
-                          setState(() {
-                            wordsArray.removeAt(rowNum);
-                          });
-                        },
-                        child: const Icon(Icons.delete),
-                      ),
-                    ]
-                );
-              }
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const SizedBox(width: 20),
+              Expanded(
+                child: TextField(
+                  controller: _inputController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter a ToDo Item',
+                    border: OutlineInputBorder(),
+
+                  ),
+                  keyboardType: TextInputType.text,
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              const SizedBox(width: 20),
+            ],
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todoList.length,
+              itemBuilder: (context, rowNum) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 20),
+                    //Text("${todoList[rowNum]} "),
+                    Text(
+                      "${todoList[rowNum].todoItem}",
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  ]);
+              }),
           ),
         ],
       ),
